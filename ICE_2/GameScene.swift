a667c208562647cfcd9d52e7b4516e8fe4a0d38c
Murtaza0726 <murtazanaqvi0726@yahoo.com>
2023-01-28 20:04:17 -0500

    added 3 cloud

--- ICE_2/GameScene.swift
+++ ICE_2/GameScene.swift
@@ -12,7 +12,7 @@ class GameScene: SKScene
     var ocean2: Ocean?
     var player: Player?
     var island: Island?
-    var cloud: Cloud?
+    var clouds: [Cloud] = []
     
     override func sceneDidLoad()
     {
@@ -42,8 +42,13 @@ class GameScene: SKScene
         addChild(island!)
         
         //Add the cloud to the scene
-        cloud = Cloud()
-        addChild(cloud!)
+        
+        for _ in 0...2
+        {
+            let cloud = Cloud()
+            clouds.append(cloud)
+            addChild(cloud)
+        }
     }
     
     func touchDown(atPoint pos : CGPoint)
@@ -88,6 +93,10 @@ class GameScene: SKScene
         ocean2?.Update()
         player?.Update()
         island?.Update()
-        cloud?.Update()
+        
+        for cloud in clouds
+        {
+            cloud.Update()
+        }
     }
 }
